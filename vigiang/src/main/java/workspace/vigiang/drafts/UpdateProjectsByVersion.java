6f9df8337186f75987a5dad67c330eb8527c3540
Jorge Almeida Junior <jorge.almeida.junior@cognyte.com>
2025-10-06 14:21:46 -0300

    Adds changes to draft

--- vigiang/src/main/java/workspace/vigiang/drafts/UpdateProjectsByVersion.java
+++ vigiang/src/main/java/workspace/vigiang/drafts/UpdateProjectsByVersion.java
@@ -16,47 +16,49 @@ import static workspace.vigiang.service.EnvironmentService.getVigiaNgPath;
 public class UpdateProjectsByVersion {
 
     public static void main(String[] args) throws IOException {
-        var VERSION = "1.7";
+        var VERSIONS = List.of("1.5", "1.7", "2.2");
         var WORK_DIR = "C:\\work\\vigiang";
 
-        Path backendPath = Paths.get(WORK_DIR + "\\" + VERSION + "\\back-" + VERSION);
-        if (!Files.exists(backendPath) || !Files.isDirectory(backendPath)) {
-            throw new IllegalArgumentException("o diretorio backendPath nao existe ou nao eh um diretorio");
-        }
-        Path frontendPath = Paths.get(WORK_DIR + "\\" + VERSION + "\\front-" + VERSION);
-        if (!Files.exists(frontendPath) || !Files.isDirectory(frontendPath)) {
-            throw new IllegalArgumentException("o diretorio frontendPath nao existe ou nao eh um diretorio");
-        }
+        for (String version : VERSIONS) {
+            Path backendPath = Paths.get(WORK_DIR + "\\" + version + "\\back-" + version);
+            if (!Files.exists(backendPath) || !Files.isDirectory(backendPath)) {
+                throw new IllegalArgumentException("o diretorio backendPath nao existe ou nao eh um diretorio");
+            }
+            Path frontendPath = Paths.get(WORK_DIR + "\\" + version + "\\front-" + version);
+            if (!Files.exists(frontendPath) || !Files.isDirectory(frontendPath)) {
+                throw new IllegalArgumentException("o diretorio frontendPath nao existe ou nao eh um diretorio");
+            }
 
 //        List<String> javaFileContents = getJavaFileContents(backendPath);
 
-        List<String> javascriptFileContents = getJavascriptFileContents(frontendPath);
+            List<String> javascriptFileContents = getJavascriptFileContents(frontendPath);
 
-        Set<String> matchesSet = new LinkedHashSet<>();
+            Set<String> matchesSet = new LinkedHashSet<>();
 
-        for (String javaFileContent : javascriptFileContents) {
-            String input = javaFileContent.trim();
+            for (String javaFileContent : javascriptFileContents) {
+                String input = javaFileContent.trim();
 
-            Pattern pattern = Pattern.compile("getConfiguration\\(['\"]([^'\"]+)['\"]");
-            Matcher matcher = pattern.matcher(input);
+                Pattern pattern = Pattern.compile("getConfiguration\\(['\"]([^'\"]+)['\"]");
+                Matcher matcher = pattern.matcher(input);
 
-            while (matcher.find()) {
-                matchesSet.add(matcher.group(1));
+                while (matcher.find()) {
+                    matchesSet.add(matcher.group(1));
+                }
             }
+
+            List<String> matches = new java.util.ArrayList<>(matchesSet);
+            matches.sort(String::compareTo);
+            String matchesString = String.join("\n", matches);
+
+            Path vigiaNgPath = getVigiaNgPath();
+            Path versionPath = Paths.get(vigiaNgPath + "\\versions\\" + version);
+            if (!Files.exists(versionPath) || !Files.isDirectory(versionPath)) {
+                throw new IllegalArgumentException("o diretorio versionPath nao existe ou nao eh um diretorio");
+            }
+
+            Path allConfigurationsPath = Paths.get(versionPath + "\\configurations.txt");
+            Files.writeString(allConfigurationsPath, matchesString);
         }
-
-        List<String> matches = new java.util.ArrayList<>(matchesSet);
-        matches.sort(String::compareTo);
-        String matchesString = String.join("\n", matches);
-
-        Path vigiaNgPath = getVigiaNgPath();
-        Path versionPath = Paths.get(vigiaNgPath + "\\versions\\" + VERSION);
-        if (!Files.exists(versionPath) || !Files.isDirectory(versionPath)) {
-            throw new IllegalArgumentException("o diretorio versionPath nao existe ou nao eh um diretorio");
-        }
-
-        Path allConfigurationsPath = Paths.get(versionPath + "\\configurations.txt");
-        Files.writeString(allConfigurationsPath, matchesString);
     }
 
     private static List<String> getJavaFileContents(Path backendPath) {
