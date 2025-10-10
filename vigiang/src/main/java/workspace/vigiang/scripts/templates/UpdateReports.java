af741e1d3d2a2556573ff488a421e5ae63eb7a1a
Jorge Almeida Junior <jorge.almeida.junior@cognyte.com>
2025-10-10 16:05:59 -0300

    Refactor UpdateReports

--- vigiang/src/main/java/workspace/vigiang/scripts/templates/UpdateReports.java
+++ vigiang/src/main/java/workspace/vigiang/scripts/templates/UpdateReports.java
@@ -1,43 +1,53 @@
 package workspace.vigiang.scripts.templates;
 
 import workspace.vigiang.dao.VigiaNgDAO;
+import workspace.vigiang.model.Carrier;
 import workspace.vigiang.model.DatabaseCredentials;
 import workspace.vigiang.service.EnvironmentService;
 
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.function.Predicate;
 
 public class UpdateReports {
 
     public static void main(String[] args) {
-        String ENVIRONMENT_NAME = "?"; // TODO: this name should be in environments
-        Integer CARRIER_ID = 0; // this id is from database
+        // the parameters bellow must match in file databases.json
+        String DATABASE_NAME = "?";
+        Carrier CARRIER = null;
+        DatabaseCredentials.Database DATABASE = null;
+
+        Integer CARRIER_ID = 1; // this id is from database
 
         try {
+            Predicate<DatabaseCredentials> databaseCredentialsPredicate = (credentials) ->
+                    credentials.getName().equals(DATABASE_NAME)
+                        && credentials.getCarrier().equals(CARRIER)
+                        && credentials.getDatabase().equals(DATABASE);
             DatabaseCredentials databaseCredentials = EnvironmentService.getVigiangDatabases().stream()
-                    .filter(credentials -> credentials.getName().equals(ENVIRONMENT_NAME))
+                    .filter(databaseCredentialsPredicate)
                     .findFirst()
-                    .orElseThrow(() -> new IllegalArgumentException("Environment not found: " + ENVIRONMENT_NAME));
+                    .orElseThrow(() -> new IllegalArgumentException("Database credentials not found, check the parameters..."));
             VigiaNgDAO dao = EnvironmentService.getVigiaNgDAO(databaseCredentials);
 
             Path reportTemplatesPath = EnvironmentService.getReportTemplatesPath(databaseCredentials);
-            try (DirectoryStream<Path> stream = Files.newDirectoryStream(reportTemplatesPath)) {
+            String carrierCode = String.format("%02d", CARRIER_ID);
+            Path carrierReportTemplatesPath = Paths.get(reportTemplatesPath + "\\" + carrierCode);
+
+            try (DirectoryStream<Path> stream = Files.newDirectoryStream(carrierReportTemplatesPath)) {
                 for (Path entry : stream) {
                     String fullFileName = entry.getFileName().toString();
                     String[] split = fullFileName.split("_");
-                    String carrierId = split[0];
-                    String reportId = split[1];
-
-                    if (!CARRIER_ID.equals(Integer.parseInt(carrierId))) continue;
+                    String reportId = split[0];
 
                     int firstUnderscore = fullFileName.indexOf('_');
-                    int secondUnderscore = fullFileName.indexOf('_', firstUnderscore + 1);
                     int lastDot = fullFileName.lastIndexOf('.');
-                    String reportName = fullFileName.substring(secondUnderscore + 1, lastDot);
+                    String reportName = fullFileName.substring(firstUnderscore + 1, lastDot);
 
                     byte[] fileBytes = Files.readAllBytes(entry);
-                    dao.updateTemplateReport(databaseCredentials, carrierId, reportId, reportName, fileBytes);
+                    dao.updateTemplateReport(databaseCredentials, String.valueOf(CARRIER_ID), reportId, reportName, fileBytes);
                 }
             }
 
