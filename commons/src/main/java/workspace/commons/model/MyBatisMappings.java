d3bc937dbc6b67a0fd1a37c7453cef70a823e4a0
Jorge Almeida Junior <jorge.almeida.junior@cognyte.com>
2025-12-29 14:09:37 -0300

    Changes UpdateMybatis

--- commons/src/main/java/workspace/commons/model/MyBatisMappings.java
+++ commons/src/main/java/workspace/commons/model/MyBatisMappings.java
@@ -1,17 +1,26 @@
 package workspace.commons.model;
 
-import lombok.Getter;
-
 import java.util.*;
 import java.util.stream.Collectors;
 
-@Getter
 public class MyBatisMappings {
 
     private final List<XmlMyBatisMapping> mappings;
     private final Map<String, Project> projects;
 
-    record Project (String name, List<XmlMyBatisMapping> mappings) { }
+    record Project (String name, List<XmlMyBatisMapping> mappings) {
+        List<XmlCallMapping> getAllCalls() {
+            return mappings.stream()
+                .flatMap(mapping -> mapping.getAllCalls().stream())
+                .toList();
+        }
+
+        List<XmlResultMap> getAllResultMaps() {
+            return mappings.stream()
+                .flatMap(mapping -> mapping.resultMaps().stream())
+                .toList();
+        }
+    }
 
     public MyBatisMappings(List<XmlMyBatisMapping> mappings) {
         validateUniqueMappings(mappings);
@@ -33,6 +42,7 @@ public class MyBatisMappings {
                 .toList();
 
             validateUniqueCalls(allCalls);
+            validateCalls(allCalls);
 
             Project project = new Project(projectName, projectMappings);
             this.projects.put(projectName, project);
@@ -81,6 +91,162 @@ public class MyBatisMappings {
             }
             seen.put(key, call);
         }
+    }
+
+    private void validateCalls(List<XmlCallMapping> allCalls) {
+        for (XmlCallMapping xmlCallMapping : allCalls) {
+            if ("".equals(xmlCallMapping.getFunctionCall()) || "".equals(xmlCallMapping.getId().trim())) {
+                throw new IllegalArgumentException(
+                    "Function call or ID cannot be empty: namespace=" + xmlCallMapping.getNamespace() +
+                    ", id=" + xmlCallMapping.getId() +
+                    ", database=" + xmlCallMapping.getDatabase()
+                );
+            }
+        }
+    }
+
+    private List<String> getProjectsKeys() {
+        List<String> projectKeys = new ArrayList<>(projects.keySet());
+        Collections.sort(projectKeys);
+        return projectKeys;
+    }
+
+    public String getMappersTxt() {
+        String resultTxt = "";
+
+        for (String projectKey : getProjectsKeys()) {
+            Project project = projects.get(projectKey);
+
+            Map<String, List<XmlCallMapping>> byNamespace = project.getAllCalls().stream()
+                .collect(Collectors.groupingBy(XmlCallMapping::getNamespace));
+            List<String> byNamespaceKeys = new ArrayList<>(byNamespace.keySet());
+            Collections.sort(byNamespaceKeys);
+
+            for (String namespace : byNamespaceKeys) {
+                List<XmlCallMapping> result = byNamespace.get(namespace);
+                result.sort(Comparator.comparing(XmlCallMapping::getId)
+                    .thenComparing(XmlCallMapping::getDatabase));
+
+                Map<String, List<XmlCallMapping>> byId = project.getAllCalls().stream()
+                    .collect(Collectors.groupingBy(XmlCallMapping::getId));
+
+                resultTxt += namespace + ":\n";
+
+                String currentId = null;
+                for (XmlCallMapping xmlCallMapping : result) {
+                    if (currentId == null || !currentId.equals(xmlCallMapping.getId())) {
+                        currentId = xmlCallMapping.getId();
+                        resultTxt += "  " + currentId + "():\n";
+
+                        var byIdList = byId.get(currentId);
+                        var oracleCall = byIdList.stream().filter(r -> "oracle".equals(r.getDatabase())).findFirst().orElse(null);
+                        var postgresCall = byIdList.stream().filter(r -> "postgres".equals(r.getDatabase())).findFirst().orElse(null);
+
+                        if (oracleCall != null) {
+                            resultTxt += "    oracle: " + oracleCall.getFunctionCall() + "\n";
+                        } else {
+                            resultTxt += "    oracle: _UNDEFINED_\n";
+                        }
+
+                        if (postgresCall != null) {
+                            resultTxt += "    postgres: " + postgresCall.getFunctionCall() + "\n";
+                        } else {
+                            resultTxt += "    postgres: _UNDEFINED_\n";
+                        }
+                    }
+                }
+                resultTxt += "\n";
+            }
+        }
+
+        return resultTxt;
+    }
+
+    public String getMappersMd() {
+        String resultMd = "";
+
+        for (String projectKey : getProjectsKeys()) {
+            Project project = projects.get(projectKey);
+
+            Map<String, List<XmlCallMapping>> byNamespace = project.getAllCalls().stream()
+                .collect(Collectors.groupingBy(XmlCallMapping::getNamespace));
+            List<String> byNamespaceKeys = new ArrayList<>(byNamespace.keySet());
+            Collections.sort(byNamespaceKeys);
+
+            Map<String, List<XmlCallMapping>> byId = project.getAllCalls().stream()
+                .collect(Collectors.groupingBy(XmlCallMapping::getId));
+
+            var resultsByNamespace = project.getAllResultMaps().stream()
+                .collect(Collectors.groupingBy(XmlResultMap::getNamespace));
+
+            for (String key : byNamespaceKeys) {
+                List<XmlCallMapping> result = byNamespace.get(key);
+                result.sort(Comparator.comparing(XmlCallMapping::getId)
+                    .thenComparing(XmlCallMapping::getDatabase));
+
+                resultMd += "# " + key + ":\n";
+                resultMd += "```\n";
+                String currentId = null;
+                for (XmlCallMapping xmlCallMapping : result) {
+                    if (currentId == null || !currentId.equals(xmlCallMapping.getId())) {
+                        currentId = xmlCallMapping.getId();
+                        resultMd += currentId + "():\n";
+
+                        var byIdList = byId.get(currentId);
+                        var oracleCall = byIdList.stream().filter(r -> "oracle".equals(r.getDatabase())).findFirst().orElse(null);
+                        var postgresCall = byIdList.stream().filter(r -> "postgres".equals(r.getDatabase())).findFirst().orElse(null);
+
+                        if (oracleCall != null) {
+                            resultMd += "  oracle: " + oracleCall.getFunctionCall() + "\n";
+                            if (!xmlCallMapping.getFunctionParams().isEmpty()) {
+                                resultMd += "    params:\n";
+                                for (String param : xmlCallMapping.getFunctionParams()) {
+                                    resultMd += "      - " + param + "\n";
+                                }
+                                resultMd += "\n";
+                            }
+                        } else {
+                            resultMd += "  oracle: _UNDEFINED_\n";
+                            resultMd += "\n";
+                        }
+
+                        if (postgresCall != null) {
+                            resultMd += "  postgres: " + postgresCall.getFunctionCall() + "\n";
+                            if (!xmlCallMapping.getFunctionParams().isEmpty()) {
+                                resultMd += "    params:\n";
+                                for (String param : xmlCallMapping.getFunctionParams()) {
+                                    resultMd += "      - " + param + "\n";
+                                }
+                                resultMd += "\n";
+                            }
+                        } else {
+                            resultMd += "  postgres: _UNDEFINED_\n";
+                            resultMd += "\n";
+                        }
+                    }
+                }
+
+                List<XmlResultMap> resultMapsForNamespace = resultsByNamespace.get(key);
+
+                if (resultMapsForNamespace != null) {
+                    resultMapsForNamespace.sort(
+                        Comparator.comparing(XmlResultMap::getId, Comparator.nullsLast(String::compareTo)));
+                    resultMd += "result_maps:\n".toUpperCase();
+
+                    for (XmlResultMap xmlResultMap : resultMapsForNamespace) {
+                        resultMd += "  " + xmlResultMap.getDatabase() + ": " + xmlResultMap.getId() + "\n";
+                        for (XmlResultMap.XmlResult xmlResult : xmlResultMap.getResults()) {
+                            resultMd += "    - property: " + xmlResult.getProperty() + ", column: " + xmlResult.getColumn() + "\n";
+                        }
+                        resultMd += "\n";
+                    }
+                }
+
+                resultMd += "```\n\n";
+            }
+        }
+
+        return resultMd;
     }
 
 }
