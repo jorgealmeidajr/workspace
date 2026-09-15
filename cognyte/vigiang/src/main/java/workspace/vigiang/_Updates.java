f8f9d99abdd661006a2968170d46564b13c8b6b8
Jorge Almeida Junior <jorge.almeida.junior@cognyte.com>
2026-09-15 14:32:31 -0300

    adds changes

---
+++ cognyte/vigiang/src/main/java/workspace/vigiang/_Updates.java
@@ -0,0 +1,12 @@
+package workspace.vigiang;
+
+public class _Updates {
+
+    public static void main(String[] args) {
+        UpdateLaboratories.main(new String[] { });
+        UpdateDatabases.main(new String[] { });
+        UpdateSchemas.main(new String[] { });
+//        UpdateProjectsByVersion.main(new String[] { });
+    }
+
+}
